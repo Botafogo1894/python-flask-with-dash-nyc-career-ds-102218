4b78ee54d22d0a8905df4df4c2cbb0a85cc95a77
Botafogo1894 <pruchka@gmail.com>
2018-11-14 23:39:28 -0500

    progress

--- ourpackage/dashboard.py
+++ ourpackage/dashboard.py
@@ -4,9 +4,16 @@ from ourpackage import app
 from ourpackage.uber_data import data
 
 
+
 # uncomment and write the code for our Dash app below:
 
-# app.layout = html.Div(children=[
-#
-#
-# ])
+app.layout = html.Div(children=[
+             html.H1("Check it out! This app has Flask AND Dash"),
+             html.Div([
+        html.P('Dash converts Python classes into HTML'),
+            html.Div([
+                dcc.Graph( id='my squish',
+                    figure= dict(data = [{'x': [1,2,3], 'y': [5,6,7]}]))
+                ])
+            ])
+])
